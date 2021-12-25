8b509505189b24a64d0939da73785a09bbd69912
noordseoostende <voropayev1@mail.ru>
2021-12-25 21:07:00 +0100

    /api/places/

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import models, serializers
 
 # Create your views here.
+
+class PlaceList(generics.ListCreateAPIView):
+  serializer_class = serializers.PlaceSerializer
+
+  def get_query(self):
+    return models.Place.objects.filter(owner_id=self.request.user.id)
+
+  def perform_create(self, seralizer):
+    seralizer.save(owner=self.request.user)
